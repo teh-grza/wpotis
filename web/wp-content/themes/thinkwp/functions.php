1b5216050f5b71c377b933d23d9c9eb77ccde4c8
Mark Hoffmann <grizzo@gmail.com>
2020-04-29 13:41:12 -0700

    update

--- web/wp-content/themes/thinkwp/functions.php
+++ web/wp-content/themes/thinkwp/functions.php
@@ -97,6 +97,7 @@ if ( ! function_exists( 'thinkwp_setup' ) ) :
 endif;
 add_action( 'after_setup_theme', 'thinkwp_setup' );
 
+
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
